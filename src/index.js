7e94f4ae588177649897f81cd4d2af2e0663b89e
Stuart Jones <stuart@savvywombat.com.au>
2022-01-25 17:49:45 +1030

    month and year settings

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { html, css, LitElement } from 'lit';
+import {html, css, LitElement} from 'lit';
 
 export class DatePicker extends LitElement {
   static get styles() {
@@ -8,30 +8,85 @@ export class DatePicker extends LitElement {
         padding: 25px;
         color: var(--date-picker-text-color, #000);
       }
+      
+      fieldset {
+        display: grid;
+        grid-template-areas:
+          ".     .     date  .     ."
+          ".     month .     year  ."
+          "main  main  main  main  main";
+        grid-template-rows: repeat(3, auto);
+        grid-template-columns: repeat(5, 1fr);
+        grid-gap: 1em;
+      }
+      
+      fieldset > .date {
+        grid-area: date;
+      }
+      
+      fieldset > .month {
+        grid-area: month;
+        display: flex;
+        justify-content: space-between;
+      }
+      
+      fieldset > .year {
+        grid-area: year;
+        display: flex;
+        justify-content: space-between
+      }
     `;
   }
 
   static get properties() {
     return {
-      title: { type: String },
-      counter: { type: Number },
+      __date: {type: Date}
     };
   }
 
   constructor() {
     super();
-    this.title = 'Hey there';
-    this.counter = 5;
+    this.date = new Date();
+    this.__date = this.date;
   }
 
-  __increment() {
-    this.counter += 1;
+  __incMonth() {
+    this.__date.setMonth(this.__date.getMonth() + 1);
+    this.__date = new Date(this.__date);
+  }
+
+  __decMonth() {
+    this.__date.setMonth(this.__date.getMonth() - 1);
+    this.__date = new Date(this.__date);
+  }
+
+  __incYear() {
+    this.__date.setFullYear(this.__date.getFullYear() + 1);
+    this.__date = new Date(this.__date);
+  }
+
+  __decYear() {
+    this.__date.setFullYear(this.__date.getFullYear() - 1);
+    this.__date = new Date(this.__date);
   }
 
   render() {
     return html`
-      <h2>${this.title} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+        <fieldset>
+            <input class="date" type="text" value="${new Intl.DateTimeFormat().format(this.__date)}">
+            
+            <div class="month">
+                <button type="button" aria-label="Go back one month" @click=${this.__decMonth}>&lt;</button>
+                <span>${new Intl.DateTimeFormat('default', {month: 'long'}).format(this.__date)}</span>
+                <button type="button" aria-label="Go forward one month" @click=${this.__incMonth}>&gt;</button>
+            </div>
+
+            <div class="year">
+                <button type="button" aria-label="Go back one year" @click=${this.__decYear}>&lt;</button>
+                <span>${new Intl.DateTimeFormat('default', {year: 'numeric'}).format(this.__date)}</span>
+                <button type="button" aria-label="Go forward one year" @click=${this.__incYear}>&gt;</button>
+            </div>
+        </fieldset>
     `;
   }
 }

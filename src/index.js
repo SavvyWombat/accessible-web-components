adb896abbbc08ee253b3a4cef5a64a638a8f76ad
Stuart Jones <stuart@savvywombat.com.au>
2022-01-27 17:01:02 +1030

    removed reliance on lit - trying for raw component

--- src/index.js
+++ src/index.js
@@ -1,92 +1,120 @@
-import {html, css, LitElement} from 'lit';
-
-export class DatePicker extends LitElement {
-  static get styles() {
-    return css`
-      :host {
-        display: block;
-        padding: 25px;
-        color: var(--date-picker-text-color, #000);
-      }
-      
-      fieldset {
-        display: grid;
-        grid-template-areas:
-          ".     .     date  .     ."
-          ".     month .     year  ."
-          "main  main  main  main  main";
-        grid-template-rows: repeat(3, auto);
-        grid-template-columns: repeat(5, 1fr);
-        grid-gap: 1em;
-      }
-      
-      fieldset > .date {
-        grid-area: date;
-      }
-      
-      fieldset > .month {
-        grid-area: month;
-        display: flex;
-        justify-content: space-between;
-      }
-      
-      fieldset > .year {
-        grid-area: year;
-        display: flex;
-        justify-content: space-between
-      }
-    `;
-  }
-
-  static get properties() {
-    return {
-      __date: {type: Date}
-    };
-  }
-
+export class DatePicker extends HTMLElement {
   constructor() {
+    // Always call super first in constructor
     super();
-    this.date = new Date();
-    this.__date = this.date;
+
+    // Element functionality written in here
+
+    this.__date = new Date();
+
+    // Create a shadow root
+    this.attachShadow({mode: 'open'}); // sets and returns 'this.shadowRoot'
+
+    // Define the fieldset wrapper
+    const datePicker = document.createElement('fieldset');
+    datePicker.setAttribute('class', 'date-picker');
+
+    // Show the current date in a disabled input
+    const date = document.createElement('input');
+    date.setAttribute('class', 'date');
+    date.setAttribute('type', 'input');
+    date.disabled = true;
+    date.setAttribute('value', new Intl.DateTimeFormat().format(this.__date));
+    datePicker.append(date);
+
+    // Selector to control the month
+    const monthSelector = document.createElement('div');
+    monthSelector.setAttribute('class', 'month');
+    monthSelector.append(this.__button(this.__decMonth, '<', 'Go back one month'));
+
+    this.monthLabel = document.createElement('span');
+    this.monthLabel.textContent = new Intl.DateTimeFormat('default', {month: 'long'}).format(this.__date)
+    monthSelector.append(this.monthLabel);
+
+    monthSelector.append(this.__button(this.__incMonth, '>', 'Go forward one month'));
+    datePicker.append(monthSelector);
+
+    // Selector to control the year
+    const yearSelector = document.createElement('div');
+    yearSelector.setAttribute('class', 'year');
+    yearSelector.append(this.__button(this.__decYear, '<', 'Go back one year'));
+
+    this.yearLabel = document.createElement('span');
+    this.yearLabel.textContent = new Intl.DateTimeFormat('default', {year: 'numeric'}).format(this.__date)
+    yearSelector.append(this.yearLabel);
+
+    yearSelector.append(this.__button(this.__incYear, '>', 'Go forward one year'));
+    datePicker.append(yearSelector);
+
+    // Add component and styles to the shadow DOM
+    this.shadowRoot.append(this.__styles(), datePicker);
   }
 
-  __incMonth() {
-    this.__date.setMonth(this.__date.getMonth() + 1);
-    this.__date = new Date(this.__date);
+  __button(listener, content, label) {
+    const button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('aria-label', label);
+    button.addEventListener('click', () => listener(this));
+    button.textContent = content;
+
+    return button;
   }
 
-  __decMonth() {
-    this.__date.setMonth(this.__date.getMonth() - 1);
-    this.__date = new Date(this.__date);
+  __decMonth(picker) {
+    picker.__date.setMonth(picker.__date.getMonth() - 1);
+    picker.__date = new Date(picker.__date);
   }
 
-  __incYear() {
-    this.__date.setFullYear(this.__date.getFullYear() + 1);
-    this.__date = new Date(this.__date);
+  __incMonth(picker) {
+    picker.__date.setMonth(picker.__date.getMonth() + 1);
+    picker.__date = new Date(picker.__date);
   }
 
-  __decYear() {
-    this.__date.setFullYear(this.__date.getFullYear() - 1);
-    this.__date = new Date(this.__date);
+  __decYear(picker) {
+    picker.__date.setFullYear(picker.__date.getFullYear() - 1);
+    picker.__date = new Date(picker.__date);
   }
 
-  render() {
-    return html`
-        <fieldset>
-            <input class="date" type="text" value="${new Intl.DateTimeFormat().format(this.__date)}">
-            
-            <div class="month">
-                <button type="button" aria-label="Go back one month" @click=${this.__decMonth}>&lt;</button>
-                <span>${new Intl.DateTimeFormat('default', {month: 'long'}).format(this.__date)}</span>
-                <button type="button" aria-label="Go forward one month" @click=${this.__incMonth}>&gt;</button>
-            </div>
+  __incYear(picker) {
+    picker.__date.setFullYear(picker.__date.getFullYear() + 1);
+    picker.__date = new Date(picker.__date);
+  }
 
-            <div class="year">
-                <button type="button" aria-label="Go back one year" @click=${this.__decYear}>&lt;</button>
-                <span>${new Intl.DateTimeFormat('default', {year: 'numeric'}).format(this.__date)}</span>
-                <button type="button" aria-label="Go forward one year" @click=${this.__incYear}>&gt;</button>
-            </div>
-        </fieldset>
-    `;
+  __styles() {
+    const styles = document.createElement('style');
+    styles.textContent = `
+.date-picker {
+  border: 1px solid black;
+  min-width: 10em;
+  min-height: 10em;
+  
+  display: grid;
+  grid-template-areas:
+    ".     .     date  .     ."
+    ".     month .     year  ."
+    "main  main  main  main  main";
+  grid-template-rows: repeat(3, auto);
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 1em;
+}
+
+.date {
+  grid-area: date;
+}
+
+.month {
+  grid-area: month;
+  display: flex;
+  justify-content: space-between;
+}
+
+.year {
+  grid-area: year;
+  display: flex;
+  justify-content: space-between;
+}
+`;
+
+    return styles;
   }
 }

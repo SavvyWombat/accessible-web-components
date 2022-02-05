74fea03ab4695223cc82d9bc3dff3ab061b68c59
Stuart Jones <stuart@savvywombat.com.au>
2022-02-05 16:38:29 +1030

    open/close behaviour

--- src/dropdown-selector.js
+++ src/dropdown-selector.js
@@ -6,17 +6,15 @@ export class DropdownSelector extends HTMLElement {
 
     this.shadowRoot.innerHTML = html;
 
+    this.ignoreBlur = false;
+    this.open = false;
+    this.activeIndex = null;
     this.selectedIndex = null;
 
+    this.__parentLabel = document.getElementById(this.getAttribute('aria-labelledby'));
     this.__label = this.shadowRoot.getElementById('label');
     this.__combobox = this.shadowRoot.getElementById('combobox');
     this.__listbox = this.shadowRoot.getElementById('listbox');
-
-    const parentLabel = document.getElementById(this.getAttribute('aria-labelledby'));
-    if (parentLabel) {
-      this.__label.textContent = parentLabel.textContent;
-      parentLabel.addEventListener('click', () => this.__combobox.focus());
-    }
 
     this.options = [...this.querySelectorAll('option')].map((option, index) => {
       return {
@@ -29,13 +27,16 @@ export class DropdownSelector extends HTMLElement {
     this.options.forEach((option, index) => {
       const element = document.createElement('div');
       element.textContent = option.label;
-      this.__listbox.appendChild(element);
 
+      element.classList.add('option');
+      element.setAttribute('id', `option-${index}`);
       element.setAttribute('aria-selected', 'false');
       if (option.selected) {
         this.selectedIndex = index;
         element.setAttribute('aria-selected', 'true');
       }
+
+      this.__listbox.appendChild(element);
     });
 
     if (this.selectedIndex === null) {
@@ -46,13 +47,62 @@ export class DropdownSelector extends HTMLElement {
       this.__combobox.textContent = this.options[this.selectedIndex].label
     }
   }
+
+  connectedCallback() {
+    if (this.isConnected) {
+      if (this.__parentLabel) {
+        this.__label.textContent = this.__parentLabel.textContent;
+
+        this.__parentLabel.addEventListener('click', this.click.bind(this));
+      }
+
+      this.__combobox.addEventListener('blur', this.blur.bind(this));
+      this.__combobox.addEventListener('click', this.click.bind(this));
+    }
+  }
+
+  disconnectedCallback() {
+    if (this.__parentLabel) {
+      this.__parentLabel.removeEventListener('click', this.click.bind(this));
+    }
+
+    this.__combobox.removeEventListener('blur', this.blur.bind(this));
+    this.__combobox.removeEventListener('click', this.click.bind(this));
+  }
+
+  click(event) {
+    this.open ? this.closeList() : this.openList();
+  }
+
+  closeList() {
+    this.open = false;
+    this.__combobox.setAttribute('aria-expanded', 'false');
+    this.__combobox.setAttribute('aria-activedescendant', '');
+
+    this.__combobox.focus();
+  }
+
+  openList() {
+    this.open = true;
+    this.__combobox.setAttribute('aria-expanded', 'true');
+
+    if (this.activeIndex === null) {
+      this.activeIndex = this.selectedIndex;
+    }
+    this.__combobox.setAttribute('aria-activedescendant', `option-${this.activeIndex}`);
+
+    this.__listbox.children[this.activeIndex].classList.add('current');
+
+    this.__combobox.focus();
+  }
 }
 
-const html = `<div>
+const html = `<div id="root">
     <label id="label"></label>
     
     <div id="combobox"
          role="combobox"
+         aria-activedescendant=""
          aria-controls="listbox"
          aria-expanded="false"
          aria-haspopup="listbox"
@@ -68,9 +118,50 @@ const html = `<div>
 </div>
 
 <style>
-    #combobox:focus {
-        border: 3px solid #666699;
+    * {
+        box-sizing: border-box;
+    }
+    
+    #root {
+        height: 3em;
+        width: fit-content;
+    }
+    
+    #label {
+        position: absolute;
+        left: -1000px;
+    }
+    
+    #combobox {
+        outline: 3px solid #999999;
         border-radius: 0.25em;
+        padding: 1em;
+    }
+    
+    #combobox:focus {
+        outline: 3px solid #9cccec;
+    }
+    
+    #listbox {
+        height: 0;
+        overflow-y: hidden;
+        position: relative;
+        background-color: #fafafa;
+    }
+    
+    #combobox[aria-expanded=true] ~ #listbox {
+        height: auto;
+        outline: 3px solid #999999;
+        border-radius: 0.25em;
+    }
+    
+    .option {
+        padding: 1em;
+    }
+    
+    .option.current,
+    .option:hover {
+        background-color: #acdcfc;
     }
 </style>
 `;
--- test/dropdown-selector.test.js
+++ test/dropdown-selector.test.js
@@ -10,17 +10,17 @@ describe('DropDownSelector', () => {
         <dropdown-selector></dropdown-selector>
     `);
 
-    const label = dropdown.shadowRoot.querySelector('#label');
+    const label = dropdown.shadowRoot.getElementById('label');
     expect(label).to.not.be.a('null');
     expect(label.textContent).to.be.a('string');
     expect(label.textContent).to.be.empty;
 
-    const combobox = dropdown.shadowRoot.querySelector('#combobox');
+    const combobox = dropdown.shadowRoot.getElementById('combobox');
     expect(combobox).to.not.be.a('null');
     expect(combobox.textContent).to.be.a('string');
     expect(combobox.textContent).to.be.empty;
 
-    const listbox = dropdown.shadowRoot.querySelector('#listbox');
+    const listbox = dropdown.shadowRoot.getElementById('listbox');
     expect(listbox).to.not.be.a('null');
     expect(listbox).to.have.property('children');
     expect(listbox.children.length).to.equal(0);
@@ -31,16 +31,17 @@ describe('DropDownSelector', () => {
         <dropdown-selector></dropdown-selector>
     `);
 
-    const combobox = dropdown.shadowRoot.querySelector('#combobox');
+    const combobox = dropdown.shadowRoot.getElementById('combobox');
     expect(combobox).to.not.be.a('null');
     expect(combobox.getAttribute('role')).to.equal('combobox');
+    expect(combobox.getAttribute('aria-activedescendant')).to.equal('');
     expect(combobox.getAttribute('aria-controls')).to.equal('listbox');
     expect(combobox.getAttribute('aria-expanded')).to.equal('false');
     expect(combobox.getAttribute('aria-haspopup')).to.equal('listbox');
     expect(combobox.getAttribute('aria-labelledby')).to.equal('label');
     expect(combobox.getAttribute('tabindex')).to.equal('0');
 
-    const listbox = dropdown.shadowRoot.querySelector('#listbox');
+    const listbox = dropdown.shadowRoot.getElementById('listbox');
     expect(listbox).to.not.be.a('null');
     expect(listbox.getAttribute('role')).to.equal('listbox');
     expect(listbox.getAttribute('aria-labelledby')).to.equal('label');
@@ -73,7 +74,7 @@ describe('DropDownSelector', () => {
     expect(dropdown.options[0].label).to.equal('Some Value');
     expect(dropdown.options[0].value).to.equal('some-value');
 
-    const listbox = dropdown.shadowRoot.querySelector('#listbox');
+    const listbox = dropdown.shadowRoot.getElementById('listbox');
     expect(listbox).to.not.be.a('null');
     expect(listbox).to.have.property('children');
     expect(listbox.children.length).to.equal(1);
@@ -102,12 +103,10 @@ describe('DropDownSelector', () => {
         </dropdown-selector>
     `);
 
-    const combobox = dropdown.shadowRoot.querySelector('#combobox');
+    const combobox = dropdown.shadowRoot.getElementById('combobox');
     expect(combobox).to.not.be.a('null');
     expect(combobox.textContent).to.equal('Some Value');
   });
-
-
 
   it('sets the combobox value to the selected option', async () => {
     const dropdown = await fixture(html`
@@ -117,8 +116,85 @@ describe('DropDownSelector', () => {
         </dropdown-selector>
     `);
 
-    const combobox = dropdown.shadowRoot.querySelector('#combobox');
+    const combobox = dropdown.shadowRoot.getElementById('combobox');
     expect(combobox).to.not.be.a('null');
     expect(combobox.textContent).to.equal('Another Value');
   });
+
+  it('starts with the listbox closed', async () => {
+    const dropdown = await fixture(html`
+        <dropdown-selector>
+            <option>Some Value</option>
+        </dropdown-selector>
+    `);
+
+    const combobox = dropdown.shadowRoot.getElementById('combobox');
+    expect(combobox.getAttribute('aria-expanded')).to.equal('false');
+
+    const listbox = dropdown.shadowRoot.getElementById('listbox');
+    expect(listbox.clientHeight).to.equal(0);
+  });
+
+  it('opens the listbox', async () => {
+    const dropdown = await fixture(html`
+        <dropdown-selector>
+            <option>Some Value</option>
+        </dropdown-selector>
+    `);
+
+    const combobox = dropdown.shadowRoot.getElementById('combobox');
+    await combobox.click();
+
+    expect(dropdown.shadowRoot.activeElement).to.equal(combobox);
+    expect(combobox.getAttribute('aria-expanded')).to.equal('true');
+
+    const listbox = dropdown.shadowRoot.getElementById('listbox');
+    expect(listbox.clientHeight).to.be.greaterThan(0);
+  });
+
+  it('sets the active descendant when opening', async() => {
+    const dropdown = await fixture(html`
+        <dropdown-selector>
+            <option>Some Value</option>
+            <option selected>Another Value</option>
+        </dropdown-selector>
+    `);
+
+    const combobox = dropdown.shadowRoot.getElementById('combobox');
+    await combobox.click();
+
+    expect(combobox.getAttribute('aria-activedescendant')).to.equal('option-1');
+  })
+
+  it('closes the list', async() => {
+    const dropdown = await fixture(html`
+        <dropdown-selector>
+            <option>Some Value</option>
+        </dropdown-selector>
+    `);
+
+    const combobox = dropdown.shadowRoot.getElementById('combobox');
+    await combobox.click(); // open
+    await combobox.click();
+
+    expect(combobox.getAttribute('aria-expanded')).to.equal('false');
+
+    const listbox = dropdown.shadowRoot.getElementById('listbox');
+    expect(listbox.clientHeight).to.equal(0);
+  })
+
+  it('unsets the active descendant when closing', async() => {
+    const dropdown = await fixture(html`
+        <dropdown-selector>
+            <option>Some Value</option>
+            <option selected>Another Value</option>
+        </dropdown-selector>
+    `);
+
+    const combobox = dropdown.shadowRoot.getElementById('combobox');
+    await combobox.click();
+    await combobox.click();
+
+    expect(combobox.getAttribute('aria-activedescendant')).to.equal('');
+  })
 });

fbbd283d3616323bccbaa6a9b191de08c9fc744a
Stuart Jones <stuart@savvywombat.com.au>
2022-03-22 17:16:58 +1030

    styling and layout for demo pages

--- demo/vue-demo/src/views/HomePage.vue
+++ demo/vue-demo/src/views/HomePage.vue
@@ -1,68 +1,100 @@
 <template>
     <header>
-        <h1>Accessible Web Components</h1>
-        <h2>Vue Demo</h2>
-
-        <form id="form">
-            <label id="label-for-dropdown">Choose a month</label>
-            <dropdown-selector aria-labelledby="label-for-dropdown">
-                <option value="0">January</option>
-                <option value="1">February</option>
-                <option value="2">March</option>
-                <option value="3">April</option>
-                <option value="4">May</option>
-                <option value="5" selected>June</option>
-                <option value="6">July</option>
-                <option value="7">August</option>
-                <option value="8">September</option>
-                <option value="9">October</option>
-                <option value="10">November</option>
-                <option value="11">December</option>
-            </dropdown-selector>
-        </form>
-
-        <p class="output">
-            <span id="output-label">Current selection</span>:
-            <output aria-labelledby="output-label">June</output>
-        </p>
+        <h2>Accessible Web Components</h2>
+        <h3>Vue Demo</h3>
     </header>
+
+    <main>
+        <section class="demo">
+            <h1>Dropdown Selector</h1>
+
+            <form id="form">
+                <label id="label-for-dropdown">Choose a month</label>
+                <dropdown-selector aria-labelledby="label-for-dropdown">
+                    <option value="0">January</option>
+                    <option value="1">February</option>
+                    <option value="2">March</option>
+                    <option value="3">April</option>
+                    <option value="4">May</option>
+                    <option value="5" selected>June</option>
+                    <option value="6">July</option>
+                    <option value="7">August</option>
+                    <option value="8">September</option>
+                    <option value="9">October</option>
+                    <option value="10">November</option>
+                    <option value="11">December</option>
+                </dropdown-selector>
+            </form>
+
+            <p class="output">
+                <span id="output-label">Current selection</span>:
+                <output aria-labelledby="output-label">June</output>
+            </p>
+        </section>
+
+        <section class="explanation">
+            <h2>User Experience</h2>
+
+            <p>
+                The dropdown for selecting the month should work according to the pattern and behaviour laid out in the
+                <a href="https://w3c.github.io/aria-practices/examples/combobox/combobox-select-only.html">
+                    Select-Only Combobox Example
+                </a> at WAI-ARIA Authoring Practices 1.2.
+            </p>
+
+            <ul>
+                <li>The dropdown is focusable with <kbd>tab</kbd></li>
+                <li>The menu will open when <kbd>up</kbd>, <kbd>down</kbd>, <kbd>enter</kbd>, or <kbd>space</kbd> are pressed when focused</li>
+                <li>The menu will open if the user starts typing</li>
+            </ul>
+
+            <p>
+                When open:
+            </p>
+
+            <ul>
+                <li>Pressing the <kbd>home</kbd> and <kbd>end</kbd> keys will highlight the first or last item respectively</li>
+                <li>Pressing the <kbd>up</kbd> or <kbd>down</kbd> keys will move the highlight accordingly</li>
+                <li>Pressing <kbd>enter</kbd> or <kbd>space</kbd> will close the menu, change the selected value to the current
+                    highlighted value, but keep focus on the dropdown
+                </li>
+                <li>Pressing the <kbd>tab</kbd> key will close the menu, change the selected value, and move focus to the next
+                    element
+                </li>
+                <li>Pressing the <kbd>escape</kbd> key will close the dropdown without changing its value</li>
+            </ul>
+        </section>
+
+        <section class="explanation">
+            <h2>Developer Experience</h2>
+
+            <p>
+                &lt;option&gt; elements work just like regular HTML Option elements.
+                The value attribute is optional, and a selected attribute can be used to set the initial value.
+            </p>
+
+            <pre><code>&lt;form id="form">
+    &lt;label id="label-for-dropdown">Choose a month&lt;/label>
+    &lt;dropdown-selector aria-labelledby="label-for-dropdown">
+        &lt;option value="0">January&lt;/option>
+        &lt;option value="1">February&lt;/option>
+        &lt;option value="2">March&lt;/option>
+        &lt;option value="3">April&lt;/option>
+        &lt;option value="4">May&lt;/option>
+        &lt;option value="5" selected>June&lt;/option>
+        &lt;option value="6">July&lt;/option>
+        &lt;option value="7">August&lt;/option>
+        &lt;option value="8">September&lt;/option>
+        &lt;option value="9">October&lt;/option>
+        &lt;option value="10">November&lt;/option>
+        &lt;option value="11">December&lt;/option>
+    &lt;/dropdown-selector>
+&lt;/form></code></pre>
+        </section>
+    </main>
+
+    <footer>
+        Accessible Web Components created by
+        <a href="https://savvywombat.com.au">Savvy Wombat</a> &copy; 2022
+    </footer>
 </template>
-
-<script>
-import { DropdownSelector } from "../../../../src/index.js";
-
-customElements.define('dropdown-selector', DropdownSelector);
-</script>
-
-<style>
-    form {
-      margin: 1rem auto;
-
-      max-width: 768px;
-
-      border: 3px solid #122436;
-      border-radius: 0.5rem;
-
-      padding: 3rem;
-    }
-
-    label {
-      display: block;
-      margin: 1rem;
-    }
-
-    p.output {
-      margin: 1rem auto;
-
-      max-width: 768px;
-
-      border: 3px solid #122436;
-      border-radius: 0.5rem;
-
-      padding: 1rem;
-    }
-
-    output {
-      margin-left: 1rem;
-    }
-</style>

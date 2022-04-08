7df5e024158574e226d8de2854afb1cc163cd33c
Stuart Jones <stuart@savvywombat.com.au>
2022-04-08 16:26:54 +0930

    react-demo is reactive

--- demo/react-demo/src/App.jsx
+++ demo/react-demo/src/App.jsx
@@ -1,15 +1,17 @@
 import {useState} from 'react'
-import '../../base.css';
-import {DropdownSelector} from "../../../src/dropdown-selector.js";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [label, setLabel] = useState('Pick a month')
+  const [numMonths, setNumMonths] = useState(12)
+  const months = [
+    'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December',
+  ]
 
   return (
     <div>
       <header>
         <h2>Accessible Web Components</h2>
-        <h3>Vue Demo</h3>
+        <h3>React Demo</h3>
       </header>
 
       <main>
@@ -18,16 +20,16 @@ function App() {
 
           <form id="form">
             <label for="edit-dropdown-label">Edit the dropdown's label</label>
-            <input id="edit-dropdown-label" type="text"/>
+            <input id="edit-dropdown-label" type="text" value={label} onChange={(event) => setLabel(event.target.value)}/>
 
             <label for="number-of-options">Edit the dropdown's label</label>
-            <input id="number-of-options" type="number" min="1" max="12"/>
+            <input id="number-of-options" type="number" min="1" max="12" value={numMonths} onChange={(event) => setNumMonths(parseInt(event.target.value))}/>
 
-            <label for="dropdown-selector">Label</label>
+            <label for="dropdown-selector">{label}</label>
             <dropdown-selector id="dropdown-selector">
-              <option>January</option>
-              <option>February</option>
-              <option>March</option>
+              { [...Array(numMonths).keys()].map((m) => (
+                  <option key={m}>{months[m]}</option>
+              ))}
             </dropdown-selector>
 
             <p className="output">
@@ -38,41 +40,6 @@ function App() {
         </section>
 
         <section className="explanation">
-          <h2>User Experience</h2>
-
-          <p>
-            The dropdown for selecting the month should work according to the pattern and behaviour laid out in the
-            <a href="https://w3c.github.io/aria-practices/examples/combobox/combobox-select-only.html">
-              Select-Only Combobox Example
-            </a> at WAI-ARIA Authoring Practices 1.2.
-          </p>
-
-          <ul>
-            <li>The dropdown is focusable with <kbd>tab</kbd></li>
-            <li>The menu will open when <kbd>up</kbd>, <kbd>down</kbd>, <kbd>enter</kbd>, or <kbd>space</kbd> are
-              pressed while focused
-            </li>
-            <li>The menu will open if the user starts typing</li>
-          </ul>
-
-          <p>
-            When open:
-          </p>
-
-          <ul>
-            <li><kbd>home</kbd> or <kbd>end</kbd> will highlight the first or last item respectively</li>
-            <li><kbd>up</kbd> or <kbd>down</kbd> will move the highlight accordingly</li>
-            <li><kbd>enter</kbd> or <kbd>space</kbd> will change the selected value to the current highlighted value,
-              close the menu, but keep focus on the dropdown
-            </li>
-            <li><kbd>tab</kbd> will change the selected value to the current highlighted value, close the menu, and then
-              move focus to the next element
-            </li>
-            <li><kbd>escape</kbd> will close the dropdown without changing its value</li>
-          </ul>
-        </section>
-
-        <section className="explanation">
           <h2>Developer Experience</h2>
 
           <p>
@@ -80,37 +47,26 @@ function App() {
             The value attribute is optional, and a selected attribute can be used to set the initial value.
           </p>
 
-          <pre><code>{`<form id="form">
-  <label id="label-for-dropdown">Choose a month</label>
-  <dropdown-selector aria-labelledby="label-for-dropdown">
-    <option value="0">January</option>
-    <option value="1">February</option>
-    <option value="2">March</option>
-    <option value="3">April</option>
-    <option value="4">May</option>
-    <option value="5" selected>June</option>
-    <option value="6">July</option>
-    <option value="7">August</option>
-    <option value="8">September</option>
-    <option value="9">October</option>
-    <option value="10">November</option>
-    <option value="11">December</option>
+          <pre><code>{`<label for="edit-dropdown-label">Edit the dropdown's label</label>
+  <input id="edit-dropdown-label" type="text" value={label} onChange={(event) => setLabel(event.target.value)}/>
+
+  <label for="number-of-options">Edit the dropdown's label</label>
+  <input id="number-of-options" type="number" min="1" max="12" value={numMonths} onChange={(event) => setNumMonths(parseInt(event.target.value))}/>
+
+  <label for="dropdown-selector">{label}</label>
+  <dropdown-selector id="dropdown-selector">
+    { [...Array(numMonths).keys()].map((m) => (
+        <option key={m}>{months[m]}</option>
+    ))}
   </dropdown-selector>
-  </form>`}</code></pre>
+
+  <p className="output">
+    <span id="output-label">Current selection</span>:
+    <output aria-labelledby="output-label">Output</output>
+  </p>
+</form>`}</code></pre>
         </section>
       </main>
-
-      <footer>
-        <p>
-          Accessible Web Components created by
-          <a href="https://savvywombat.com.au">Savvy Wombat</a> &copy; 2022
-        </p>
-
-        <p>
-          Source code available on
-          <a href="https://github.com/SavvyWombat/accessible-web-components">GitHub</a>
-        </p>
-      </footer>
     </div>
   )
 }
--- demo/react-demo/src/main.jsx
+++ demo/react-demo/src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import '../../base.css'
 import App from './App'
+
+import {DropdownSelector} from "../../../src/dropdown-selector.js";
+
+customElements.define('dropdown-selector', DropdownSelector);
 
 ReactDOM.render(
   <React.StrictMode>

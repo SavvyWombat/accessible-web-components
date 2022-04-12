85e6d1d3a6590e4a3ef08355a913e153d14cd142
Stuart Jones <stuart@savvywombat.com.au>
2022-04-13 07:58:47 +0930

    404 page

---
+++ docs/source/404.blade.php
@@ -0,0 +1,9 @@
+---
+title: 404 - Not Found
+---
+
+<div class="flex flex-col gap-16 mt-8 my-16">
+    <section class="p-6 bg-gray-100 shadow-inner rounded-2xl">
+        <h1 class="text-3xl font-bold text-blue-500">404 - Not Found</h1>
+    </section>
+</div>

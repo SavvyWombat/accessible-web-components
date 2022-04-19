30f96e32705ab2d0656fbe225fc2513743dc36e0
Stuart Jones <stuart@savvywombat.com.au>
2022-04-19 14:59:35 +0930

    easier to see what links are in page on the left

--- docs/source/_assets/js/inPageLinks.js
+++ docs/source/_assets/js/inPageLinks.js
@@ -1,24 +1,28 @@
 export const inPageLinks = () => {
-  const pathname = window.location.pathname === '/' ? '/' : window.location.pathname.replace(/\/$/, '');
-  const pageLink = document.querySelector(`nav a[href="${pathname}"]`);
   const headings = document.querySelectorAll('main h2');
 
-  if (headings.length > 0) {
-    const template = new DOMParser().parseFromString(`<a class="in-page"/>`, 'text/html').body.firstChild;
-
-    let previousNode = pageLink;
-    headings.forEach((heading) => {
-      const link = document.importNode(template);
-
-      heading.id = heading.textContent
-        .toLowerCase()
-        .replace(/\W+/g, '-');
-
-      link.href = `#${heading.id}`;
-      link.textContent = heading.textContent;
-
-      previousNode.after(link);
-      previousNode = link;
-    });
+  if (headings.length === 0) {
+    return;
   }
+
+  const pathname = window.location.pathname === '/' ? '/' : window.location.pathname.replace(/\/$/, '');
+  const pageLink = document.querySelector(`nav a[href="${pathname}"]`);
+  const inPageLinks = document.importNode(new DOMParser().parseFromString(`<div class="in-page"/>`, 'text/html').body.firstChild);
+  pageLink.after(inPageLinks);
+  inPageLinks.append(pageLink);
+
+  const template = new DOMParser().parseFromString(`<a class="in-page"/>`, 'text/html').body.firstChild;
+
+  headings.forEach((heading) => {
+    const link = document.importNode(template);
+
+    heading.id = heading.textContent
+      .toLowerCase()
+      .replace(/\W+/g, '-');
+
+    link.href = `#${heading.id}`;
+    link.textContent = heading.textContent;
+
+    inPageLinks.appendChild(link);
+  });
 };

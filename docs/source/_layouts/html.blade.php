a02b78858186747de980f78839ac5045e0a3b7c0
Stuart Jones <stuart@savvywombat.com.au>
2022-04-19 16:42:53 +0930

    moar favicons

--- docs/source/_layouts/html.blade.php
+++ docs/source/_layouts/html.blade.php
@@ -17,6 +17,9 @@
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
 
         <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="192x192" href="/assets/images/favicon-192x192.png">
+        <link rel="icon" type="image/png" sizes="96x96" href="/assets/images/favicon-96x96.png">
+        <link rel="icon" type="image/png" sizes="48x48" href="/assets/images/favicon-48x48.png">
         <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">

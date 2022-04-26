64c219bc54bd1f9851620b16a9f33b0e2784f8b5
Stuart Jones <stuart@savvywombat.com.au>
2022-04-26 18:41:30 +0930

    Started StyledComponent documentation

--- docs/source/component-mixins/styled-component.blade.php
+++ docs/source/component-mixins/styled-component.blade.php
@@ -12,4 +12,71 @@ title: StyledComponent
             The <code class="js-class">StyledComponent</code> mixin enables Web Components to pull in style rules, making customisation simpler.
         </p>
     </section>
+
+    <section>
+        <h2>Usage</h2>
+
+        <h3>Extending from StyledComponent in JavaScript</h3>
+
+        <pre>
+            <x-torchlight-code language="javascript">
+import {StyleComponent} from 'accessible-web-components';
+
+class DropdownSelector extends StyleComponent(HTMLElement) {
+  constructor() {
+    super()
+      .attachShadow({mode: 'open'})
+      .innerHTML = `<div></div>`;
+
+     // other initialisation
+  }
+
+  connectedCallback() {
+    if (this.isConnected) {
+      super.connectedCallback();
+
+    // other initialisation requiring access to DOM and ShadowDOM
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+
+    // other cleanup
+  }
+}
+            </x-torchlight-code>
+        </pre>
+    </section>
+
+    <section>
+        <h2>CSS Selectors</h2>
+
+        <pre>
+            <x-torchlight-code language="html">
+<dropdown-selector id="target-by-id" class="pretty-selector">
+</dropdown-selector>
+            </x-torchlight-code>
+        </pre>
+
+        <pre>
+            <x-torchlight-code language="css">
+dropdown-selector {
+  background-color: gray;
+}
+
+.pretty-selector {
+  border: 1px solid firebrick;
+}
+
+#target-by-id {
+  font-size: 2rem;
+}
+            </x-torchlight-code>
+        </pre>
+
+        <p>
+            Style rules targeting the element, class or ID of a <code class="js-class">StyledComponent</code>
+            will be applied to the root element of the Web Component, maintaining the same specificity as the original rule.
+        </p>
+    </section>
 @endsection

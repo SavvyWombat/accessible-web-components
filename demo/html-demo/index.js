ea2cbb1ab54c41644cd63ab068cb612111469c2e
Stuart Jones <stuart@savvywombat.com.au>
2022-04-29 16:52:51 +0930

    add tab-group to html-demo

---
+++ demo/html-demo/index.js
@@ -0,0 +1,10 @@
+import '../base.css';
+
+import favicon from '../../images/logo.svg';
+
+document.getElementsByTagName('head').item(0).insertAdjacentHTML('beforeend',
+  `<link rel="icon" type="image/svg" href="${favicon}">`
+);
+
+import {defineAccessibleWebComponents} from '../../src/index.js';
+defineAccessibleWebComponents();

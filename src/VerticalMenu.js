d142dccd30fc8049bc066151e22293c682e83649
Stuart Jones <stuart@savvywombat.com.au>
2022-11-03 17:49:46 +1030

    vertical-menu: up, down, home, end

--- src/VerticalMenu.js
+++ src/VerticalMenu.js
@@ -1,4 +1,6 @@
-export class VerticalMenu extends HTMLElement {
+import {StyledComponent} from "./StyledComponent.js";
+
+export class VerticalMenu extends StyledComponent(HTMLElement) {
   constructor() {
     super()
       .attachShadow({mode: 'open'})
@@ -7,17 +9,112 @@ export class VerticalMenu extends HTMLElement {
 
   connectedCallback() {
     if (this.isConnected) {
+      super.connectedCallback();
+
       this.__root = this.shadowRoot.getElementById('root');
 
       this.__root.innerHTML = this.innerHTML;
+
+      this.__root.addEventListener('keydown', this.keydown.bind(this));
     }
   }
 
   disconnectedCallback() {
+    super.disconnectedCallback();
 
+    this.__root.removeEventListener('keydown', this.keydown.bind(this));
+  }
+
+  keydown(event) {
+    const action = this.__actionFromKey(event);
+
+    if (action) {
+      event.preventDefault();
+    }
+
+    switch (action) {
+      case Actions.NextLink:
+        this.__nextLink(event.target)?.focus();
+        break;
+      case Actions.PrevLink:
+        this.__prevLink(event.target)?.focus();
+        break;
+      case Actions.StepOut:
+        this.__parentLink(event.target)?.focus();
+        break;
+      case Actions.FirstLink:
+        this.__root.querySelectorAll('a')[0].focus();
+        break;
+      case Actions.LastLink:
+        this.__root.querySelectorAll('a')[this.__root.querySelectorAll('a').length - 1].focus();
+        break;
+    }
+  }
+
+  __actionFromKey(event) {
+    const {key, altKey, ctrlKey, metaKey} = event;
+
+    if (key === 'Home') {
+      return Actions.FirstLink;
+    }
+
+    if (key === 'End') {
+      return Actions.LastLink;
+    }
+
+    if (key === 'ArrowDown') {
+      return Actions.NextLink;
+    }
+
+    if (key === 'ArrowUp') {
+      return Actions.PrevLink;
+    }
+
+    if (key === 'ArrowLeft') {
+      return Actions.StepOut;
+    }
+  }
+
+  __nextLink(target) {
+    const {nextLink} = Array.from(this.__root.querySelectorAll('a')).reduce(({nextLink, previous}, current) => {
+      if (!nextLink) {
+        if (previous === target) {
+          nextLink = current;
+        }
+      }
+
+      return {nextLink, previous: current};
+    }, {nextLink: null, previous: null});
+
+    return nextLink;
+  }
+
+  __parentLink(target) {
+
+    return null;
+  }
+
+  __prevLink(target) {
+    const {prevLink} = Array.from(this.__root.querySelectorAll('a')).reduce(({prevLink, previous}, current) => {
+      if (current === target) {
+        prevLink = previous;
+      }
+
+      return {prevLink, previous: current}
+    }, {prevLink: null, previous: null});
+
+    return prevLink;
   }
 }
 
-const html = `<div id="root">
+const Actions = {
+  NextLink: 1,
+  PrevLink: 2,
+  StepOut: 3,
+  FirstLink: 4,
+  LastLink: 5,
+}
 
-</div>`;
+const html = `<nav id="root">
+
+</nav>`;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import {DropdownSelector} from './DropdownSelector.js';
-import {VerticalMenu} from './VerticalMenu.js';
 import {TabCard} from './TabCard.js';
 import {TabGroup} from './TabGroup.js';
+import {VerticalMenu} from './VerticalMenu.js';
 
 export {LabelledComponent} from './LabelledComponent.js';
 export {StyledComponent} from './StyledComponent.js';
 
 export {
   DropdownSelector,
-  VerticalMenu,
   TabCard,
-  TabGroup
+  TabGroup,
+  VerticalMenu
 };
 
 export const defineAccessibleWebComponents = () => {
   customElements.define('dropdown-selector', DropdownSelector);
-  customElements.define('vertical-menu', VerticalMenu);
   customElements.define('tab-card', TabCard);
   customElements.define('tab-group', TabGroup);
+  customElements.define('vertical-menu', VerticalMenu);
 }

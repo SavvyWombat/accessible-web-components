ba450b249ef86eeb9845c836a66c0adc1224c6b1
Stuart Jones <stuart@savvywombat.com.au>
2022-11-12 09:14:17 +1030

    fix menu for mobile (it can open and has no odd artifacts around it)

--- docs/source/_partials/navigation.blade.php
+++ docs/source/_partials/navigation.blade.php
@@ -1,7 +1,8 @@
-<vertical-menu class="grid-in-nav sticky top-0 overscroll-none text-gray-100 bg-blue-900 p-4 pr-8">
+<vertical-menu class="grid-in-nav sticky top-0 overscroll-none text-gray-100 bg-blue-900 p-4 pr-8 medium:p-0">
     <nav>
         <span id="site-navigation-control"
-              class="wide:hidden block no-underline text-gray-100 rounded
+              class="wide:hidden block no-underline text-gray-100 rounded bg-blue-900
+                  medium:px-3 medium:py-2
                   outline-offset-0
                   focus:no-underline focus:outline focus:outline-4 focus:outline-blue-300
                   hover:no-underline hover:outline hover:outline-4 hover:outline-blue-300 hover:bg-blue-300 hover:text-blue-900
@@ -32,3 +33,55 @@
         </ul>
     </nav>
 </vertical-menu>
+
+<script>
+    const control = document.getElementById('site-navigation-control');
+    const menu = document.getElementById('site-navigation');
+
+    const toggleMenu = () => {
+        if (menu.clientHeight) {
+            menu.style.display = 'none';
+            menu.setAttribute('aria-expanded', 'false');
+        } else {
+            menu.style.display = 'flex';
+            menu.setAttribute('aria-expanded', 'true');
+        }
+    }
+
+    const closeMenu = () => {
+        if (control.clientHeight) {
+            menu.style.display = 'none';
+            menu.setAttribute('aria-expanded', 'false');
+        }
+    }
+
+    control.addEventListener('click', toggleMenu);
+
+    control.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            toggleMenu();
+        }
+        if (event.key === 'Escape') {
+            closeMenu();
+        }
+    });
+
+    menu.addEventListener('keydown', (event) => {
+        if (event.key === 'Escape') {
+            closeMenu();
+        }
+    })
+
+    const resizeObserver = new ResizeObserver(entries => {
+        if (entries[0].target.clientHeight === 0) {
+            menu.style.display = '';
+            menu.removeAttribute('aria-expanded');
+        } else {
+            if (! menu.hasAttribute('aria-expanded')) {
+                menu.setAttribute('aria-expanded', 'false');
+            }
+        }
+    });
+
+    resizeObserver.observe(control);
+</script>
